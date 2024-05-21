020797d52249867bb45de892023833facb35f3c2
BJZJZJZJ <66ap66ap@gmail.com>
2024-05-21 17:39:56 +0900

    0521 db test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from'./components/Navbar';
+import Home from'./pages/Home'; // 추가 된 내용
+import Jaso from'./pages/Jaso';
+import Resume from'./pages/Resume';
 
 function App() {
-    
 
     return (
       <>
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route exact path='/' element={<Home />} />
+          
+          <Route path='/jaso' element={<Jaso />} />
+          <Route path='/resume' element={<Resume />} />
         </Routes>
       </Router>
       </>
---
+++ src/pages/Jaso.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class jaso extends Component {
+
+    render() {
+      return (
+          <div>
+              <h3> jaso </h3>
+          </div>
+      );
+    }
+  }
+  
+  export default jaso;
