6def0fdc0599fcb4773dc91babbf241f7b570532
BJZJZJZJ <66ap66ap@gmail.com>
2024-05-22 17:34:26 +0900

    살려야만한다2

---
+++ src/pages/signUp.js
@@ -0,0 +1,319 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import { Card as MuiCard } from '@mui/material';
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+
+import getSignUpTheme from '../components/getSignUpTheme';
+import ToggleColorMode from '../components/ToggleColorMode';
+import { GoogleIcon, FacebookIcon } from '../components/CustomIcons';
+import { redirect } from 'react-router-dom';
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Toggle design language"
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>Material Design 2</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
+}
+
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  gap: theme.spacing(4),
+  width: '100%',
+  padding: theme.spacing(2),
+  boxShadow:
+    theme.palette.mode === 'light'
+      ? 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px, hsla(220, 30%, 5%, 0.05) 0px 0px 0px 1px'
+      : 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px, hsla(220, 30%, 5%, 0.05) 0px 0px 0px 1px',
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+    width: '450px',
+  },
+}));
+
+const SignUpContainer = styled(Stack)(({ theme }) => ({
+  height: 'auto',
+  padingBottom: theme.spacing(12),
+  backgroundImage:
+    theme.palette.mode === 'light'
+      ? 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))'
+      : 'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.3), hsl(220, 30%, 5%))',
+  backgroundRepeat: 'no-repeat',
+  [theme.breakpoints.up('sm')]: {
+    paddingBottom: 0,
+    height: '100dvh',
+  },
+}));
+
+export default function SignUp() {
+  const [mode, setMode] = React.useState('dark');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const defaultTheme = createTheme({ palette: { mode } });
+  const SignUpTheme = createTheme(getSignUpTheme(mode));
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+  const [nameError, setNameError] = React.useState(false);
+  const [nameErrorMessage, setNameErrorMessage] = React.useState('');
+
+  const validateInputs = () => {
+    const email = document.getElementById('userEmail');
+    const password = document.getElementById('userPassword');
+    const name = document.getElementById('userName');
+
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    if (!password.value || password.value.length < 6) {
+      setPasswordError(true);
+      setPasswordErrorMessage('Password must be at least 6 characters long.');
+      isValid = false;
+    } else {
+      setPasswordError(false);
+      setPasswordErrorMessage('');
+    }
+
+    if (!name.value || name.value.length < 1) {
+      setNameError(true);
+      setNameErrorMessage('Name is required.');
+      isValid = false;
+    } else {
+      setNameError(false);
+      setNameErrorMessage('');
+    }
+
+    return isValid;
+  };
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const userData = {
+      userName: data.get('userName'),
+      userEmail: data.get('userEmail'),
+      userPassword: data.get('userPassword')
+    }
+
+    fetch("http://localhost:3001/signup", { //signin 주소에서 받을 예정
+          method: "post", // method :통신방법
+          headers: {      // headers: API 응답에 대한 정보를 담음
+            "content-type": "application/json",
+          },
+          body: JSON.stringify(userData), //userData라는 객체를 보냄
+        })
+          .then((res) => res.json())
+          .then((json) => {
+            if(json.isSuccess==="True"){
+              alert('회원가입이 완료되었습니다!')
+            }
+            else{
+              alert(json.isSuccess)
+            }
+          });
+  };
+
+  return (
+    <ThemeProvider theme={showCustomTheme ? SignUpTheme : defaultTheme}>
+      <CssBaseline />
+      <SignUpContainer direction="column" justifyContent="space-between">
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{
+            position: { xs: 'static', sm: 'fixed' },
+            width: '100%',
+            p: { xs: 2, sm: 4 },
+          }}
+        >
+          <Button
+            startIcon={<ArrowBackRoundedIcon />}
+            component="a"
+            href="/boardAllList"
+          >
+            Back
+          </Button>
+          <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+        </Stack>
+        <Stack
+          justifyContent="center"
+          sx={{ height: { xs: '100%', sm: '100dvh' }, p: 2 }}
+        >
+          <Card>
+        
+            <Typography
+              component="h1"
+              variant="h4"
+              sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+            >
+             회원가입
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+            >
+              <FormControl>
+                <FormLabel htmlFor="userName">성명</FormLabel>
+                <TextField
+                  autoComplete="userName"
+                  name="userName"
+                  required
+                  fullWidth
+                  id="userName"
+                  placeholder="홍길동"
+                  error={nameError}
+                  helperText={nameErrorMessage}
+                  color={nameError ? 'error' : 'primary'}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="userEmail">이메일</FormLabel>
+                <TextField
+                  required
+                  fullWidth
+                  id="userEmail"
+                  placeholder="your@email.com"
+                  name="userEmail"
+                  autoComplete="userEmail"
+                  variant="outlined"
+                  error={emailError}
+                  helperText={emailErrorMessage}
+                  color={passwordError ? 'error' : 'primary'}
+                />
+              </FormControl>
+              <FormControl>
+                <FormLabel htmlFor="userPassword">비밀번호</FormLabel>
+                <TextField
+                  required
+                  fullWidth
+                  name="userPassword"
+                  placeholder="••••••"
+                  type="password"
+                  id="userPassword"
+                  autoComplete="new-password"
+                  variant="outlined"
+                  error={passwordError}
+                  helperText={passwordErrorMessage}
+                  color={passwordError ? 'error' : 'primary'}
+                />
+              </FormControl>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="홍보 메일 받기를 희망합니다."
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                onClick={validateInputs}
+              >
+                가입 하기
+              </Button>
+              <Link
+                href="sign-in/"
+                variant="body2"
+                sx={{ alignSelf: 'center' }}
+              >
+                이미 아이디가 있으신가요? 로그인 하기
+              </Link>
+            </Box>
+            <Divider>
+              <Typography color="text.secondary">or</Typography>
+            </Divider>
+            <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                color="secondary"
+                onClick={() => alert('Sign up with Google')}
+                startIcon={<GoogleIcon />}
+              >
+                구글로 회원가입 하기
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="outlined"
+                color="secondary"
+                onClick={() => alert('Sign up with Facebook')}
+                startIcon={<FacebookIcon />}
+              >
+                페이스북으로 회원가입 하기
+              </Button>
+            </Box>
+          </Card>
+        </Stack>
+      </SignUpContainer>
+
+    </ThemeProvider>
+  );
+}
