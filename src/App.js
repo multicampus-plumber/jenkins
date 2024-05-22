4c1c417e5bc51148013774c1876b8e39ca647753
J-BJ <54877831+BJZJZJZJ@users.noreply.github.com>
2024-05-22 09:29:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from'./components/Navbar';
-import Home from'./pages/Home'; // 추가 된 내용
+import Home from'./pages/Home';
 import Jaso from'./pages/Jaso';
 import Resume from'./pages/Resume';
 
