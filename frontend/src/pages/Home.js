05bc0dbc2c4954eb19133dd3891794a2398d2554
KimGunWoo <112372296+hatacon97@users.noreply.github.com>
2024-05-23 16:08:45 +0900

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,150 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, Component } from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Stack from '@mui/material/Stack';
+import { Card as MuiCard } from '@mui/material';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
-class Home extends Component {
 
-    render() {
-      return (
-          <div>
-              <h3> Home </h3>
-          </div>
-      );
-    }
-  }
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import AutoAwesomeRoundedIcon from '@mui/icons-material/AutoAwesomeRounded';
+
+import getSignInTheme from '../components/getSignInTheme';
+import ToggleColorMode from '../components/ToggleColorMode';
+
+import CommonTable from '../components/CommonTable';
+import CommonTableColumn from '../components/CommonTableColumn';
+import CommonTableRow from '../components/CommonTableRow';
+
+
+function ToggleCustomTheme({ showCustomTheme, toggleCustomTheme }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '100dvw',
+        position: 'fixed',
+        bottom: 24,
+      }}
+    >
+      <ToggleButtonGroup
+        color="primary"
+        exclusive
+        value={showCustomTheme}
+        onChange={toggleCustomTheme}
+        aria-label="Toggle design language"
+        sx={{
+          backgroundColor: 'background.default',
+          '& .Mui-selected': {
+            pointerEvents: 'none',
+          },
+        }}
+      >
+        <ToggleButton value>
+          <AutoAwesomeRoundedIcon sx={{ fontSize: '20px', mr: 1 }} />
+          Custom theme
+        </ToggleButton>
+        <ToggleButton value={false}>Material Design 2</ToggleButton>
+      </ToggleButtonGroup>
+    </Box>
+  );
+}
+
+ToggleCustomTheme.propTypes = {
+  showCustomTheme: PropTypes.shape({
+    valueOf: PropTypes.func.isRequired,
+  }).isRequired,
+  toggleCustomTheme: PropTypes.func.isRequired,
+};
+
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  gap: theme.spacing(4),
+  width: '100%',
+  padding: theme.spacing(2),
+  boxShadow:
+    theme.palette.mode === 'light'
+      ? 'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px, hsla(220, 30%, 5%, 0.05) 0px 0px 0px 1px'
+      : 'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px, hsla(220, 30%, 5%, 0.05) 0px 0px 0px 1px',
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+    width: '450px',
+  },
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: 'auto',
+  padingBottom: theme.spacing(12),
+  backgroundImage:
+    theme.palette.mode === 'light'
+      ? 'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))'
+      : 'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.3), hsl(220, 30%, 5%))',
+  backgroundRepeat: 'no-repeat',
+  [theme.breakpoints.up('sm')]: {
+    paddingBottom: 0,
+    height: '100dvh',
+  },
+}));
+
+function Home() {
+
+  const [mode, setMode] = React.useState('dark');
+  const [showCustomTheme, setShowCustomTheme] = React.useState(true);
+  const defaultTheme = createTheme({ palette: { mode } });
+  const SignInTheme = createTheme(getSignInTheme(mode));
+
+
+  const toggleColorMode = () => {
+    setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+  };
+
+
+  return (<>
+
+<ThemeProvider theme={showCustomTheme ? SignInTheme : defaultTheme}>
+      <CssBaseline />
+
+       
+      <SignInContainer direction="column" justifyContent="space-between">
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          sx={{
+            position: { xs: 'static', sm: 'fixed' },
+            width: '100%',
+            p: { xs: 2, sm: 4 },
+          }}
+        >
+          <Button
+            startIcon={<ArrowBackRoundedIcon />}
+            component="a"
+            href="/"
+          >
+            Back
+          </Button>
+          <ToggleColorMode mode={mode} toggleColorMode={toggleColorMode} />
+        </Stack>
+        <Stack>
+          <Paper >
+            <div class="MuiPaper-root" >
+                <center> <h3>Home</h3> </center>
+            </div>
+          </Paper>
+        </Stack>
+      </SignInContainer>
+    </ThemeProvider>
+
+  </>);
+}
   
-  export default Home;
+export default Home;
