9b8d6a7a237f4af6d0ab31d5b9c034c342154e44
KimGunWoo <112372296+hatacon97@users.noreply.github.com>
2024-05-23 16:08:23 +0900

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,78 +1,25 @@
-import React, { useState, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import {Button} from './Button';
-import '../css/Navbar.css';
-
-function Navbar() {
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-
-
-    const handleClick = () => setClick(!click) ;
-    const closeMobileMenu = () => setClick(false);
-
-    // 화면 크기에 따라서 버튼이 보이고 안보이도록 설정한다. 
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false)
-        }
-        else {
-            setButton(true);
-        }
-    };
-
-    // SIGNUP버튼이 사이즈가 줄어들면 없어지도록 한다. 
-    useEffect(() => {
-        showButton();
-    }, []);
-
-
-    window.addEventListener('resize', showButton);
-
-    return (
-        <>
-        <nav className = 'navbar'>
-            <div className = 'navbar-container'>
-                {/* 모바일버전에서 클릭하면 메뉴 보이도록 설정하는 것도 한다. (close Mobile Menu)는 다시 버튼 누르면 없어지고 생기고 하도록 한다.  */}
-                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                    Toyseven-Request 
-                    <i className='fab fa-typo3' />
-                </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className = {click ? 'fas fa-times' : 'fas fa-bars' } />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to='/' className='nav-links' onClick = {closeMobileMenu}>
-                            홈
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/notice' className='nav-links' onClick = {closeMobileMenu}>
-                            공지사항
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/jaso' className='nav-links' onClick = {closeMobileMenu}>
-                            자기소개서
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/interview' className='nav-links' onClick = {closeMobileMenu}>
-                            면접후기
-                        </Link>
-                    </li>
-                    <li className='nav-item'>
-                        <Link to='/sign-in' className='nav-links-mobile' onClick = {closeMobileMenu}>
-                            Sign In
-                        </Link>
-                    </li>
-                </ul>
-                {button && <Button buttonStyle='btn--outline'>회원가입</Button>}
-            </div>
-        </nav>
-        </>
-    );
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Link from '@mui/material/Link';
+export default function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link href="/" underline="none" color="inherit">Home</Link>
+          </Typography>
+          <Button color="inherit" href='/jaso'>자기소개서</Button>
+          <Button color="inherit" href='/interview'>면접 후기</Button>
+          <Button color="inherit" href='/sign-in'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
-
-export default Navbar
